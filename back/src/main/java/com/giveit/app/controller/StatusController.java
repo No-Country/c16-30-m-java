a4ce9bf7952badfc52217835113e4964b017f590
Adonis Castellanos <adoniscastellanos1@gmail.com>
2024-02-13 11:26:46 -0400

    add status endpoint and application properties and Dokerfile

---
+++ back/src/main/java/com/giveit/app/controller/StatusController.java
@@ -0,0 +1,14 @@
+package com.giveit.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @GetMapping("/status")
+    public String getStatus() {
+        return "OK";
+    }
+
+}
--- back/src/main/java/com/giveit/app/entity/User.java
+++ back/src/main/java/com/giveit/app/entity/User.java
@@ -1,7 +1,14 @@
 package com.giveit.app.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
+    @Id
+    private Long id;
+
+    private String name;
 }
