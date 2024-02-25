ca4e9c084541f32354667e49926e5eb034a2c09e
danilo2504 <danilobautista2004@gmail.com>
2024-02-25 22:12:31 +0100

    add hero to home and modal to publish

---
+++ front/src/components/PublishModal/PublishModal.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import HashLink from "../HashLink";
+
+const PublishModal = () => {
+  return (
+    <div className="bg-[#fff] w-[420px] rounded-2xl absolute bottom-8 right-10 font-main box-shadow-hero">
+      <div className="mt-[30px] mb-[16px] mx-[18px] flex flex-col justify-between">
+        <p className="font-normal leading-[35px] text-[18px] tracking-[1.2px] mb-[20px]">
+          Ofrece todo lo que ya no usas, conectá con tus vecinos y ayuda al
+          planeta!
+        </p>
+        <div className="flex items-center justify-between">
+          <HashLink
+            to="/legalities#features"
+            className="text-[12px] font-bold underline"
+          >
+            ¿Como funciona GiveIt?
+          </HashLink>
+          <NavLink
+            to="/publish"
+            className="bg-primary text-white py-[8px] rounded-[100px] px-[50px] font-bold hover:brightness-110 transition-[filter] ease-in duration-200"
+          >
+            Publicar
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PublishModal;
