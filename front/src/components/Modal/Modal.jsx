b0244fd14cf4eaadcb99a49b8a3fdfd520d8c915
Gaston095 <Gastonsalas095@gmail.com>
2024-03-01 10:50:11 -0300

    Se arregla error en el diseño del login

--- front/src/components/Modal/Modal.jsx
+++ front/src/components/Modal/Modal.jsx
@@ -10,8 +10,8 @@ export default function Modal({ isOpen, setIsOpen }) {
   return (
     <>
       {isOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
-          <div className="bi bi-x-lg bg-white relative w-[738px] h-[836px] p-5 rounded flex flex-col justify-center items-center gap-5">
+        <div className="fixed z-10 inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
+          <div className="bi bi-x-lg bg-white relative w-[738px] h-full p-5 rounded flex flex-col justify-center items-center gap-5">
             <svg
               onClick={() => {
                 setIsOpen(false);
