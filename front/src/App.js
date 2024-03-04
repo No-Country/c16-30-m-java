a193aad01db513559ccbaffef0e7cdded351fc48
danilo2504 <danilobautista2004@gmail.com>
2024-03-04 20:01:16 +0100

    configure product context and quit conosle log from getProductById

--- front/src/App.js
+++ front/src/App.js
@@ -12,23 +12,26 @@ import {
 } from "./views";
 import DetailView from "./views/DetailView/DetailView";
 import { Layout } from "./layout";
+import { ProductsProvider } from "./contexts/ProductsContext";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route element={<Layout />}>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/legalities" element={<Legalities />} />
-          <Route path="/micuenta" element={<MiCuenta />} />
-          <Route path="/mispublicaciones" element={<MisPublicaciones />} />
-          <Route path="/detailview/:productId" element={<DetailView />} />
-          <Route path="/mensajes" element={<Mensajes />} />
-          <Route path="/publish" element={<Publish />} />
-        </Route>
+        <ProductsProvider>
+          <Route element={<Layout />}>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/legalities" element={<Legalities />} />
+            <Route path="/micuenta" element={<MiCuenta />} />
+            <Route path="/mispublicaciones" element={<MisPublicaciones />} />
+            <Route path="/detailview/:productId" element={<DetailView />} />
+            <Route path="/mensajes" element={<Mensajes />} />
+            <Route path="/publish" element={<Publish />} />
+          </Route>
+        </ProductsProvider>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
